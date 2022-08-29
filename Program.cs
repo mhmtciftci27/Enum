458c23fae3a20d608b817a6111a675b9b4094cd8
mhmtciftci <mhmtciftci@protonmail.com>
2022-08-29 17:29:22 +0300

    updated

--- Program.cs
+++ Program.cs
@@ -2,6 +2,32 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(Gunler.Pazar);
+        Console.WriteLine((int)Gunler.Cumartesi);
+
+        int sıcaklık =32;
+        if(sıcaklık <= (int)HavaDurumu.Normal)
+            Console.WriteLine("Havanın ısnmasını bekle");
+        else if(sıcaklık>=(int)HavaDurumu.Sıcak)
+            Console.WriteLine("Havan Sıcak");  
+        else if(sıcaklık>=(int)HavaDurumu.Normal && sıcaklık>=(int)HavaDurumu.CokSıcak)
+            Console.WriteLine("dışarı çıkalım");
     }
+}
+enum Gunler
+{
+    Pazartesi=1,
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma,
+    Cumartesi,
+    Pazar
+}
+enum HavaDurumu
+{
+    Soğuk =5,
+    Normal=20,
+    Sıcak = 25,
+    CokSıcak = 30
 }
